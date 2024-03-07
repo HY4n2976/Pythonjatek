5f95ce332cf66ca831eb8f7bb0d55a089ec4add6
HY4n2976 <zsombor.vereb@gmail.com>
2024-03-07 12:26:32 +0100

    kiirtam a paklit, elkezdtema karyta kiosztast

--- BlackJackDos.py
+++ BlackJackDos.py
@@ -1,5 +1,9 @@
+import random
 class FeketeJani:
     def __init__(self, pakli):
         self.Value = int(pakli[0])
         self.Color = pakli[1]
-        self.Name = pakli[2]
+        self.Name = pakli[2].strip("\n")
+
+    def Kartyak(self):
+        print("Az on kartyai:", )
--- BlackJackUno.py
+++ BlackJackUno.py
@@ -3,3 +3,11 @@ import random
 
 file = open("pakli.txt", "r", encoding="utf8")
 
+pakli = []
+
+for sor in file:
+    pakli.append(BlackJackDos.FeketeJani(sor.split("\t")))
+
+print("Osszes kartyak",len(pakli))
+for i in range(len(pakli)):
+    print(pakli[i].Value, pakli[i].Color, pakli[i].Name)
