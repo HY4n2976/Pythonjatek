2c7613ca2b76808ea46694839fcde8df908a7216
HY4n2976 <zsombor.vereb@gmail.com>
2024-03-18 09:31:52 +0100

    kiosztottam a jatekos es a dealer kartyait

--- BlackJackDos.py
+++ BlackJackDos.py
@@ -1,10 +1,7 @@
 import random
-class FeketeJani:
+class Jackie:
     def __init__(self, pakli):
         self.Value = int(pakli[0])
         self.Color = pakli[1]
         self.Name = pakli[2].strip("\n")
-
-    def Kartyak(self):
-        print("Az on kartyai:", )
 
--- BlackJackUno.py
+++ BlackJackUno.py
@@ -6,15 +6,20 @@ file = open("pakli.txt", "r", encoding="utf8")
 pakli = []
 
 for sor in file:
-    pakli.append(BlackJackDos.FeketeJani(sor.split("\t")))
+    pakli.append(BlackJackDos.Jackie(sor.split("\t")))
 
-print("Osszes kartyak",len(pakli))
-for i in range(len(pakli)):
-    print(pakli[i].Value, pakli[i].Color, pakli[i].Name)
+#print("Osszes kartyak",len(pakli))
+#for i in range(len(pakli)):
+    #print(pakli[i].Value, pakli[i].Color, pakli[i].Name)
 
-random = random.randint(0, len(pakli)-1)
+print("Az on kartyai:")
+for i in range(2):
+    vlami = random.randint(0, len(pakli) - 1)
+    print(pakli[vlami].Value, pakli[vlami].Color, pakli[vlami].Name)
+    pakli.pop(vlami)
 
-for i in range(len(pakli)):
-
-
-print("A kártyái" ,random)
+print("A dealer kartyai:")
+for i in range(2):
+    vlami = random.randint(0, len(pakli) - 1)
+    print(pakli[vlami].Value, pakli[vlami].Color, pakli[vlami].Name)
+    pakli.pop(vlami)
