55b94516432e3739936c3615a18a531f8e5c3523
HY4XN <zsombor.vereb@gmail.com>
2024-03-21 17:39:36 +0100

    befejeztem a jatekot a jatekos csak a dealer ellen tud jatszani

--- BlackJackUno.py
+++ BlackJackUno.py
@@ -39,31 +39,68 @@ for i in range(2):
 
 print("A dealer kartyainak erteke:",dealer)
 
-milegyen = int(input("Mit szeretnel tenni?, Ha kersz lapot: 0, ha double down: 1, ha bedobod: 2"))
-
-while milegyen < 0 or milegyen > 2:
-    print("Megint hulye vagy ember!")
-    milegyen = int(input("Mit akarsz csinalni?!, Ha kersz lapot: 0, ha double down: 1, ha bedobod: 2"))
-
-if milegyen == 0:
-    vlami = random.randint(0, len(pakli) - 1)
-    print("Az uj kartyad:", pakli[vlami].Value, pakli[vlami].Color, pakli[vlami].Name)
-    sajat += pakli[vlami].Value
-    pakli.pop(vlami)
-    milegyen = int(input("Mit szeretnel tenni?, Ha kersz lapot: 0, ha double down: 1, ha bedobod: 2"))
+if dealer == 21:
+    print("A dealer nyert sajna a penzedet elbuktad :/")
+elif sajat == 21:
+    print("Nyerteeell!!! A nyeremenyed osszege:", penz * 2)
+else:
+    milegyen = int(input("Mit szeretnel tenni?, Ha kersz lapot: 0, ha double down: 1, ha varsz: 2 "))
 
     while milegyen < 0 or milegyen > 2:
         print("Megint hulye vagy ember!")
-        milegyen = int(input("Mit akarsz csinalni?!, Ha kersz lapot: 0, ha double down: 1, ha bedobod: 2"))
+        milegyen = int(input("Mit akarsz csinalni?!, Ha kersz lapot: 0, ha double down: 1, ha varsz: 2 "))
 
-    print("Kartyaid uj erteke:", sajat)
-if milegyen == 1:
-    vlami = random.randint(0, len(pakli) - 1)
-    print("Az uj kartyad:", pakli[vlami].Value, pakli[vlami].Color, pakli[vlami].Name)
-    sajat += pakli[vlami].Value
-    pakli.pop(vlami)
-    print("Kartyaid uj erteke:", sajat)
+    if milegyen == 0:
+        vlami = random.randint(0, len(pakli) - 1)
+        print("Az uj kartyad:", pakli[vlami].Value, pakli[vlami].Color, pakli[vlami].Name)
+        sajat += pakli[vlami].Value
+        pakli.pop(vlami)
+        print("Kartyaid uj erteke:", sajat)
 
-if milegyen == 2:
-    print("Jatek vege. Sajna vesztettel, de ha nem dobtad volna be akkor lehet nyertel volna :/")
+    while milegyen < 0 or milegyen > 2:
+        print("Kikeszitesz komolyan ˘`-´˘")
+        milegyen = int(input("Mitcsinalsz?!, Ha kersz lapot: 0, ha double down: 1, ha varsz: 2 "))
 
+    if sajat < 21 and not milegyen == 1 and not milegyen == 2:
+        milegyen = int(input("Mit szeretnel tenni?, Ha kersz lapot: 0, ha double down: 1, ha varsz: 2 "))
+    elif sajat == 21:
+        print("Nyerteeell!!! A nyeremenyed osszege:", penz*2)
+    elif sajat > 21:
+        print("Vesztettel nem folytatodik a jatek es meg a penzed is elvesztetted hahaa bena vagy XD!!")
+
+    while milegyen < 0 or milegyen > 2:
+        print("Meg mindig nem birsz magaddal ugye? hat jo ¯\_(ツ)_/¯")
+        milegyen = int(input("Deee mit szeretnel csinalni?, Ha kersz lapot: 0, ha double down: 1, ha varsz: 2 "))
+
+    if milegyen == 1:
+        vlami = random.randint(0, len(pakli) - 1)
+        print("Az uj kartyad:", pakli[vlami].Value, pakli[vlami].Color, pakli[vlami].Name)
+        sajat += pakli[vlami].Value
+        pakli.pop(vlami)
+        print("Kartyaid uj erteke:", sajat)
+        print("Kockaztatott penzosszeg:", penz*2)
+        if sajat > 21:
+            print("Hahhhaaaaa elbuktaadd azthitted majd jol megszeded magad penzzel mi? Haaatt nem XDD!!")
+
+
+    if milegyen == 2 or milegyen == 1 and not sajat >= 21:
+        print("Na varj egy picit nem egyedul jatszasz °-°")
+        if dealer <= 17:
+            masik = random.randint(0, len(pakli) - 1)
+            print("A dealer uj kartyaja:",pakli[masik].Value, pakli[masik].Color, pakli[masik].Name)
+            dealer += pakli[masik].Value
+            pakli.pop(masik)
+            print("A dealer kartyainak erteke:", dealer)
+            if dealer > 21 or dealer < sajat:
+                print("Nyertel Ocsipook a nyeremenyed:", penz*2)
+            elif dealer == 21:
+                print("A dealer nyert sajna a penzedet elbuktad :/")
+            elif dealer < sajat and dealer < 21:
+                print("Szep volt te nyerteeell! A nyeremenyed:", penz*2)
+        elif dealer >= 17 and dealer < 21:
+            if dealer > sajat:
+                 print("Nemar a Dealer nyert, a penzed is eluszott sok sikert legkozelebb :(")
+            elif sajat > dealer :
+                    print("Yippiee te nyerteell a nyert penzosszeg:", penz*2)
+            elif sajat == dealer:
+                print("A kartyaitok erteke egyenlo, igy dontetlen lett es vissza kapod a penzed :D")
